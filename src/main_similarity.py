1f80868fc715e3b5430a8c4e7013e8cd68a3861a
Mazen-Serli <mazen.natour@serli.com>
2025-01-20 17:03:24 +0100

    Add env Vars

--- src/main_similarity.py
+++ src/main_similarity.py
@@ -1,12 +1,16 @@
 # https://github.com/totogot/ImageSimilarity
-
+import os
 import image_similarity as imgsim
+from dotenv import load_dotenv
+load_dotenv()
 
 ImgSim = imgsim.Img2Vec('resnet50', weights='DEFAULT')
+DATASET_PATH = os.getenv('DATASET_PATH')
+TARGET_PATH = os.getenv('TARGET_PATH')
 
 # Path to the data set
-ImgSim.embed_dataset("path")
+ImgSim.embed_dataset(DATASET_PATH)
 ImgSim.dataset
 
 # Path to the image to search similarity
-ImgSim.similar_images("path to image", n=5)
+ImgSim.similar_images(TARGET_PATH, n=5)
