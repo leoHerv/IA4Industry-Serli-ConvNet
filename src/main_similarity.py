ec4ca46e12267fcd8cb57639b93003ba71376078
Léo Hervouet <leoohervouet@gmail.com>
2025-01-20 16:28:39 +0100

    Remove leooh path

--- src/main_similarity.py
+++ src/main_similarity.py
@@ -5,10 +5,8 @@ import image_similarity as imgsim
 ImgSim = imgsim.Img2Vec('resnet50', weights='DEFAULT')
 
 # Path to the data set
-#ImgSim.embed_dataset("path")
-ImgSim.embed_dataset("C:/Users/leooh/Documents/Ecole/Universite/M2/IA4Industry/dataset/test3/")
+ImgSim.embed_dataset("path")
 ImgSim.dataset
 
 # Path to the image to search similarity
-# ImgSim.similar_images("path to image", n=5)
-ImgSim.similar_images("C:/Users/leooh/Documents/Ecole/Universite/M2/IA4Industry/dataset/part009/frame_9036.png", n=5)
+ImgSim.similar_images("path to image", n=5)
