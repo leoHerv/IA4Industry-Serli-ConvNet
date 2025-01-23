dde1aa43c1864f91c367bd94397f15d8e0f19111
Haalt <edgarhaltwill@gmail.com>
2025-01-23 14:32:33 +0100

    b7

--- src/model/kart_model.py
+++ src/model/kart_model.py
@@ -5,14 +5,18 @@ import torchvision.models as models
 import torch.optim as optim
 import torchvision.transforms as T
 import copy
-
+from codecarbon import EmissionsTracker
 
 from src.dataset.dataset import MyDataset
+
+tracker = EmissionsTracker()
+tracker.start()
+
 
 BATCH_SIZE = 32
 
 transform = T.Compose([
-    T.Resize((380, 380)),  # typical input size for ResNet
+    T.Resize((600, 600)),
     T.ToTensor(),
 ])
 
@@ -20,24 +24,25 @@ dataset = MyDataset(csv_file='dataset.csv', root_dir='./resizeDataSet', transfor
 
 total_size = len(dataset)
 train_size = int(0.70 * total_size)
-val_size   = int(0.15 * total_size)
-test_size  = total_size - train_size - val_size
+val_size   = total_size - train_size
+# val_size   = int(0.15 * total_size)
+# test_size  = total_size - train_size - val_size
 
-train_dataset, val_dataset, test_dataset = random_split(
+train_dataset, val_dataset = random_split(
     dataset,
-    [train_size, val_size, test_size]
+    [train_size, val_size]
 )
 
 train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 val_loader   = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
-test_loader  = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+# test_loader  = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
 class KartResNet50(nn.Module):
     def __init__(self, pretrained=True):
         super(KartResNet50, self).__init__()
 
         # self.backbone = models.resnet50(weights=models.ResNet50_Weights.DEFAULT if pretrained else None)
-        self.backbone = models.efficientnet_b4(weights=models.EfficientNet_B4_Weights.DEFAULT)
+        self.backbone = models.efficientnet_b7(weights=models.EfficientNet_B7_Weights.DEFAULT)
 
         # remove top
         self.backbone.classifier = nn.Identity()
@@ -47,7 +52,7 @@ class KartResNet50(nn.Module):
             param.requires_grad = False
 
         self.classifier = nn.Sequential(
-            nn.Linear(1792, 1024),
+            nn.Linear(2560, 1024),
             nn.ReLU(),
             nn.Linear(1024, 128),
             nn.ReLU(),
@@ -68,8 +73,9 @@ model = KartResNet50(pretrained=True)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
 
-criterion = nn.MSELoss() # TODO: correct loss function
-optimizer = optim.Adam(model.classifier.parameters(), lr=1e-3)
+# criterion = nn.MSELoss()
+criterion = nn.L1Loss()
+optimizer = optim.Adam(model.classifier.parameters(), lr=1e-2)
 
 
 num_epochs = 50
@@ -86,7 +92,7 @@ for epoch in range(num_epochs):
     model.train()
     running_train_loss = 0.0
     correct_train = 0
-    running_train_mae = 0.0
+    running_train_mse = 0.0
 
     for images, labels in train_loader:
         images = images.to(device)
@@ -102,16 +108,16 @@ for epoch in range(num_epochs):
         running_train_loss += loss.item() * images.size(0)
 
 
-        batch_mae = torch.mean(torch.abs(outputs - labels), dim=1)
-        running_train_mae += batch_mae.sum().item()
+        batch_mse = torch.mean((outputs - labels) ** 2, dim=1)
+        running_train_mse += batch_mse.sum().item()
 
     epoch_train_loss = running_train_loss / len(train_loader.dataset)
-    epoch_train_mae = running_train_mae / len(train_loader.dataset)
+    epoch_train_mse = running_train_mse / len(train_loader.dataset)
 
 
     model.eval()
     running_val_loss = 0.0
-    running_val_mae = 0.0
+    running_val_mse = 0.0
     with torch.no_grad():
         for images, labels in val_loader:
             images = images.to(device)
@@ -122,17 +128,17 @@ for epoch in range(num_epochs):
             running_val_loss += loss.item() * images.size(0)
 
 
-            batch_mae = torch.mean(torch.abs(outputs - labels), dim=1)
-            running_val_mae += batch_mae.sum().item()
+            batch_mse = torch.mean((outputs - labels) ** 2, dim=1)
+            running_val_mse += batch_mse.sum().item()
 
     epoch_val_loss = running_val_loss / len(val_loader.dataset)
-    epoch_val_mae = running_val_mae / len(val_loader.dataset)
+    epoch_val_mse = running_val_mse / len(val_loader.dataset)
 
 
 
     print(f'Epoch [{epoch+1}/{num_epochs}] '
-          f'Train Loss: {epoch_train_loss:.4f} | Train MAE: {epoch_train_mae:.4f} '
-          f'Val Loss: {epoch_val_loss:.4f} | Val MAE: {epoch_val_mae:.4f}')
+          f'Train Loss: {epoch_train_loss:.4f} | Train MAE: {epoch_train_mse:.4f} '
+          f'Val Loss: {epoch_val_loss:.4f} | Val MAE: {epoch_val_mse:.4f}')
     
     if epoch_val_loss < best_val_loss:
         best_val_loss = epoch_val_loss
@@ -149,10 +155,11 @@ for epoch in range(num_epochs):
         print(f"Early stopping triggered after {epoch+1} epochs!")
         break
 
+tracker.stop()
 
 model.load_state_dict(best_model_weights)
 
-dummy_input = torch.randn(1, 3, 380, 380, device=device)
+dummy_input = torch.randn(1, 3, 600, 600, device=device)
 
 onnx_model_path = "kart_resnet50.onnx"
 
@@ -173,25 +180,25 @@ torch.onnx.export(
 
 print(f"Model has been exported to {onnx_model_path}")
 
-model.eval()
-running_test_loss = 0.0
-running_test_mae = 0.0
-with torch.no_grad():
-    for images, labels in test_loader:
-        images = images.to(device)
-        labels = labels.to(device)
+# model.eval()
+# running_test_loss = 0.0
+# running_test_mae = 0.0
+# with torch.no_grad():
+#     for images, labels in test_loader:
+#         images = images.to(device)
+#         labels = labels.to(device)
 
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-        running_test_loss += loss.item() * images.size(0)
-
-
-        batch_mae = torch.mean(torch.abs(outputs - labels), dim=1)
-        running_test_mae += batch_mae.sum().item()
-
-epoch_test_loss = running_test_loss / len(test_loader.dataset)
-epoch_test_mae = running_test_mae / len(test_loader.dataset)
+#         outputs = model(images)
+#         loss = criterion(outputs, labels)
+#         running_test_loss += loss.item() * images.size(0)
 
 
-print(f'Test Loss: {epoch_test_loss:.4f} | Test MAE: {epoch_test_mae:.4f}')
+#         batch_mae = torch.mean(torch.abs(outputs - labels), dim=1)
+#         running_test_mae += batch_mae.sum().item()
+
+# epoch_test_loss = running_test_loss / len(test_loader.dataset)
+# epoch_test_mae = running_test_mae / len(test_loader.dataset)
+
+
+# print(f'Test Loss: {epoch_test_loss:.4f} | Test MAE: {epoch_test_mae:.4f}')
 
